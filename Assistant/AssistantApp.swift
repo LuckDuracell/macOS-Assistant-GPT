64b5867ac8f001f65578426179c964a92722a648
LuckDev <lukedru@yahoo.com>
2023-02-25 16:51:14 -0800

    Initial Commit

---
+++ Assistant/AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistantApp.swift
+//  Assistant
+//
+//  Created by Luke Drushell on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
